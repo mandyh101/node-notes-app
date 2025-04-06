b547b6cfddc16f37b2c3652dd820cfd333469781
mandy <mandyhale10@gmail.com>
2025-04-07 11:23:27 +1200

    feat: add new note in cli working

--- src/command.js
+++ src/command.js
@@ -1,6 +1,7 @@
 // code for the CLI
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { newNote } from "./notes.js";
 
 yargs(hideBin(process.argv))
   .command('new <note>', 'Create a new note', yargs => {
@@ -8,44 +9,46 @@ yargs(hideBin(process.argv))
       type: 'string',
       describe: 'The content of the note to create'
     })
-  }, (argv) =>{
-    console.info(argv.note)
+  }, async (argv) => {
+    const tags = argv.tags ? argv.tags.split(',') : [];
+    const note = await newNote(argv.note, tags);
+    console.log(' new note!', note);
   })
   .option('tag', {
     alias: 't',
     type: 'string',
     description: 'Tags to add to the note'
   })
-  .command('all', 'get all notes', () => {}, async(argv) => {
-    // WIP
-  })
-  .command('find <filter>', 'get matching notes', yargs => {
-    return yargs.positional('filter', {
-      type: 'string',
-      describe: 'The search term to filter notes.content by'
-    })
-  }, async(argv) => {
-      //WIP
-  })
-  .command('remove', '<id>', 'remove a note by id', yargs => {
-    return yargs.positional('id', {
-      type: 'string',
-      describe: 'The id of the note to remove'
-    })
-  }, async(argv) => {
-    //WIP
-  })
-  .command('web [port]', 'Launch the web interface to see notes', yargs => {
-    return yargs.positional('port', {
-      type: 'number',
-      describe: 'The port to launch the web interface on',
-      default: 5000,
-    })
-  }, async(argv) => {
-    //WIP
-  })
-  .command('clean', 'Delete all notes', () => {}, async(argv) => {
-    //WIP
-  })
+  // .command('all', 'get all notes', () => {}, async(argv) => {
+  //   // WIP
+  // })
+  // .command('find <filter>', 'get matching notes', yargs => {
+  //   return yargs.positional('filter', {
+  //     type: 'string',
+  //     describe: 'The search term to filter notes.content by'
+  //   })
+  // }, async(argv) => {
+  //     //WIP
+  // })
+  // .command('remove', '<id>', 'remove a note by id', yargs => {
+  //   return yargs.positional('id', {
+  //     type: 'string',
+  //     describe: 'The id of the note to remove'
+  //   })
+  // }, async(argv) => {
+  //   //WIP
+  // })
+  // .command('web [port]', 'Launch the web interface to see notes', yargs => {
+  //   return yargs.positional('port', {
+  //     type: 'number',
+  //     describe: 'The port to launch the web interface on',
+  //     default: 5000,
+  //   })
+  // }, async(argv) => {
+  //   //WIP
+  // })
+  // .command('clean', 'Delete all notes', () => {}, async(argv) => {
+  //   //WIP
+  // })
   .demandCommand(1)
   .parse()
