55054d67952247136400ab6b886d7622e180092e
mandy <mandyhale10@gmail.com>
2025-04-06 21:08:21 +1200

    WIP running the CLI to create a new note - including notes on working with the process object and it's argv property

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A CLI based Note taking app built with Node.js
 ## Getting started
 Run `npm link` to install the notes-app cli in this project inside the bin folder in your computer. 
 - type `which notes-app` to confirm you have access to the notes-app cli in your bin folder.
+
+## Commands
+Once linked, you can begin using the CLI with `notes-app` as your prefix. e.g. running `notes-app` in the terminal will execute your javascipt file with node.
+
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-console.log("hello world")
+console.log(process.argv)
+
+// initialise our note variable as the third argument passed into the process args property.
+// first arg is always the absolute path to the Node.js executable (e.g., /Users/myname/.nvm/versions/node/v21.7.3/bin/node).
+// second arg is always the absolute path to the JavaScript file being executed (e.g., /Users/myname/.nvm/versions/node/v21.7.3/bin/notes-app).
+const note = process.argv[2]
+//our data model
+const newNote = {
+  content: note,
+  id: Date.now()
+}
