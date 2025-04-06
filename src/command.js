4703a9cfe41af0a71a960abdffc62f4df751249b
mandy <mandyhale10@gmail.com>
2025-04-07 09:53:05 +1200

    feat: build out commands with yarg - tag option - find - remove - web - clean

--- src/command.js
+++ src/command.js
@@ -6,7 +6,7 @@ yargs(hideBin(process.argv))
   .command('new <note>', 'Create a new note', yargs => {
     return yargs.positional('note', {
       type: 'string',
-      description: 'Note to be created'
+      describe: 'The content of the note to create'
     })
   }, (argv) =>{
     console.info(argv.note)
@@ -15,6 +15,37 @@ yargs(hideBin(process.argv))
     alias: 't',
     type: 'string',
     description: 'Tags to add to the note'
+  })
+  .command('all', 'get all notes', () => {}, async(argv) => {
+    // WIP
+  })
+  .command('find <filter>', 'get matching notes', yargs => {
+    return yargs.positional('filter', {
+      type: 'string',
+      describe: 'The search term to filter notes.content by'
+    })
+  }, async(argv) => {
+      //WIP
+  })
+  .command('remove', '<id>', 'remove a note by id', yargs => {
+    return yargs.positional('id', {
+      type: 'string',
+      describe: 'The id of the note to remove'
+    })
+  }, async(argv) => {
+    //WIP
+  })
+  .command('web [port]', 'Launch the web interface to see notes', yargs => {
+    return yargs.positional('port', {
+      type: 'number',
+      describe: 'The port to launch the web interface on',
+      default: 5000,
+    })
+  }, async(argv) => {
+    //WIP
+  })
+  .command('clean', 'Delete all notes', () => {}, async(argv) => {
+    //WIP
   })
   .demandCommand(1)
   .parse()
